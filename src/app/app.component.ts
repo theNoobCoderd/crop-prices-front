6d99ce003b7cc5cc56fb3af5ea404113b5a2c9fd
ismailramjean <ismailramjean@gmail.com>
2024-06-07 23:40:54 +0400

    & small changes - added footer - increased whatsapp size

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, HostListener} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {ItemTableComponent} from "./components/item-table/item-table.component";
+import {NgClass} from "@angular/common";
 
 @Component({
 	selector: 'app-root',
 	standalone: true,
-	imports: [RouterOutlet, ItemTableComponent],
+	imports: [RouterOutlet, ItemTableComponent, NgClass],
 	templateUrl: './app.component.html',
 	styleUrl: './app.component.less',
 })
 export class AppComponent {
+
+	showElement = false;
+
+	@HostListener('window:scroll', [])
+	onWindowScroll() {
+		const element = document.querySelector('.brand-name');
+		if (element) {
+			const rect = element.getBoundingClientRect();
+			this.showElement = rect.bottom <= 50;
+		}
+	}
 }
