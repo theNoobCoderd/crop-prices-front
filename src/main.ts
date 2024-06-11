0ff2b0b3c6e923dcc9caef8dc635827b48fea380
ismailramjean <ismailramjean@gmail.com>
2024-06-11 22:17:21 +0400

    & added web analytics - added posthog library

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import posthog from "posthog-js";
+
+posthog.init(
+	'phc_iVhxqcsJlUSE3HT568Yt3cCYvmTuxQtpTmSMANJF75X',
+	{
+		api_host:'https://us.i.posthog.com',
+		person_profiles: 'identified_only'
+	}
+)
 
 bootstrapApplication(AppComponent, appConfig)
 	.catch((err) => console.error(err));
