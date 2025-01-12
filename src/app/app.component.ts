1e2e9a77fc50587c2ea071bac128d2ef030c9459
ismailramjean <ismailramjean@gmail.com>
2025-01-12 21:51:42 +0400

    & general updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,35 @@
 import {Component, HostListener, inject, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {ItemTableComponent} from "./components/item-table/item-table.component";
-import {NgClass} from "@angular/common";
 import {BrandNameComponent} from "./components/brand-name/brand-name.component";
 import {MainNavComponent} from "./components/nagivation/main-nav/main-nav.component";
 import {AuthService} from "./services/authentication/auth.service";
+import {UserService} from "./services/user/user.service";
+import {NgToastComponent} from "./components/ng-toast/ng-toast.component";
 
 @Component({
 	selector: 'app-root',
 	standalone: true,
-	imports: [RouterOutlet, ItemTableComponent, NgClass, BrandNameComponent, MainNavComponent],
+	imports: [RouterOutlet, BrandNameComponent, MainNavComponent, NgToastComponent],
 	templateUrl: './app.component.html',
 	styleUrl: './app.component.less',
 })
 export class AppComponent implements OnInit {
 	authService = inject(AuthService);
+	userService = inject(UserService);
 
 	showElement = false;
+
+	private _currentUser: string  | null = null;
 
 	ngOnInit(): void {
 		this.authService.firebaseUser$.subscribe(firebaseUser => {
 			console.log("user signal", firebaseUser);
-		})
+		});
+
+		this._currentUser = localStorage.getItem("currentUser");
+		if (this._currentUser) {
+			this.userService.currentUser$.next(JSON.parse(this._currentUser));
+		}
 	}
 
 	@HostListener('window:scroll', [])
