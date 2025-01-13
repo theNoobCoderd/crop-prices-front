f491e704de3081f6e48585e79c5f0d6ef4582c88
ismailramjean <ismailramjean@gmail.com>
2025-01-13 18:29:54 +0400

    & angular 18 update

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,7 @@ import {ApplicationConfig} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import {provideHttpClient} from "@angular/common/http";
+import { provideHttpClient } from "@angular/common/http";
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 
