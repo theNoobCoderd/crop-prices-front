1605cab4c8c79050960c2cccdf64b2711511284e
ismailramjean <ismailramjean@gmail.com>
2025-02-11 01:16:21 +0400

    & some changes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -9,16 +9,6 @@ import {provideCharts, withDefaultRegisterables} from "ng2-charts";
 import {getStorage, provideStorage} from "@angular/fire/storage";
 import {FIREBASE_OPTIONS} from "@angular/fire/compat";
 
-const firebaseConfig = {
-	apiKey: "AIzaSyARgGd6p_9GsJwcn1tCU24eNztYUpdEQVc",
-	authDomain: "fir-crops-main.firebaseapp.com",
-	projectId: "fir-crops-main",
-	storageBucket: "fir-crops-main.firebasestorage.app",
-	messagingSenderId: "701697537930",
-	appId: "1:701697537930:web:d9d0733c0fbe51a4a3ba73",
-	measurementId: "G-K28Q77JLBH"
-};
-
 export const appConfig: ApplicationConfig = {
 	providers: [provideRouter(routes),
 		provideHttpClient(),
