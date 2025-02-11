cc980d6bcbe1019c3cf30310b07ac7b6fe6432bb
ismailramjean <ismailramjean@gmail.com>
2025-02-11 14:02:36 +0400

    & added bootstrap carousel

--- src/app/components/item-table/item-table.component.ts
+++ src/app/components/item-table/item-table.component.ts
@@ -1,13 +1,15 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {CropsService} from "../../services/crops/crops.service";
 import {Vegetable} from "../../models/vegetable.model";
 import {AsyncPipe, NgSwitch, NgSwitchCase} from "@angular/common";
 import {NavigationComponent} from "../nagivation/navigation.component";
-import {Observable} from "rxjs";
+import {Observable, of} from "rxjs";
 import {ExtractionDate} from "../../models/extraction-date.model";
 import {Unit} from "../../models/unit.enum";
 import {ReplaceBatonPipe} from "../../pipes/replace-baton.pipe";
 import {NgxSkeletonLoaderModule} from "ngx-skeleton-loader";
+import {NgbCarouselModule} from "@ng-bootstrap/ng-bootstrap";
+import {GRAPH_ONE} from "../../../assets/mocks/mock-data";
 
 @Component({
 	selector: 'app-item-table',
@@ -18,16 +20,21 @@ import {NgxSkeletonLoaderModule} from "ngx-skeleton-loader";
 		ReplaceBatonPipe,
 		NgxSkeletonLoaderModule,
 		NgSwitch,
-		NgSwitchCase
+		NgSwitchCase,
+		NgbCarouselModule
 	],
 	templateUrl: './item-table.component.html',
 	styleUrl: './item-table.component.less'
 })
-export class ItemTableComponent {
+export class ItemTableComponent implements OnInit {
 	vegetables$: Observable<Vegetable[]> | undefined
 
 	constructor(private _cropsService: CropsService) {
 	}
+
+	ngOnInit(): void {
+        this.currentDate();
+    }
 
 	nextDate(nextDate: ExtractionDate): void {
 		this.vegetables$ = this._cropsService.getVegetableByDate(nextDate);
@@ -37,8 +44,9 @@ export class ItemTableComponent {
 		this.vegetables$ = this._cropsService.getVegetableByDate(previousDate);
 	}
 
-	currentDate(previousDate: ExtractionDate): void {
-		this.vegetables$ = this._cropsService.getVegetableByDate(previousDate);
+	currentDate(previousDate?: ExtractionDate): void {
+		// this.vegetables$ = this._cropsService.getVegetableByDate(previousDate);
+		this.vegetables$ = of(GRAPH_ONE);
 	}
 
 	protected readonly Unit = Unit;
