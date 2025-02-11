1fafdc21177ae295dc654d077cd482337d6002df
ismailramjean <ismailramjean@gmail.com>
2025-02-11 17:00:25 +0400

    & added environment variables for firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,10 @@
 export const environment = {
 	production: true,
-	apiUrl: "https://api.kozpri.com:8443"  // Production API URL
+	apiUrl: "https://api.kozpri.com:8443", // Production API URL
+	fireApiKey: process.env["FIREBASE_API_KEY"],
+	fireAuthDomain: process.env["FIREBASE_AUTH_DOMAIN"],
+	fireProjectId: process.env["FIREBASE_PROJECT_ID"],
+	fireStorageBucket: process.env["FIREBASE_STORAGE_BUCKET"],
+	fireMessageSenderId: process.env["FIREBASE_MESSAGE_SENDER_ID"],
+	fireAppId: process.env["FIREBASE_APP_ID"]
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,10 @@
 export const environment = {
 	production: false,
-	apiUrl: "http://localhost:8080"  // Development API URL
+	apiUrl: "http://localhost:8080",  // Development API URL
+	fireApiKey: process.env["FIREBASE_API_KEY"],
+	fireAuthDomain: process.env["FIREBASE_AUTH_DOMAIN"],
+	fireProjectId: process.env["FIREBASE_PROJECT_ID"],
+	fireStorageBucket: process.env["FIREBASE_STORAGE_BUCKET"],
+	fireMessageSenderId: process.env["FIREBASE_MESSAGE_SENDER_ID"],
+	fireAppId: process.env["FIREBASE_APP_ID"]
 };
