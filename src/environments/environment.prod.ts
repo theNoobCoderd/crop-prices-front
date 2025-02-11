315f44bf667e00cda13a9133a904f23bbe5a5577
ismailramjean <ismailramjean@gmail.com>
2025-02-11 17:21:26 +0400

    & added config fixes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,8 +3,8 @@ export const environment = {
 	apiUrl: "https://api.kozpri.com:8443", // Production API URL
 	fireApiKey: process.env["FIREBASE_API_KEY"],
 	fireAuthDomain: process.env["FIREBASE_AUTH_DOMAIN"],
-	fireProjectId: process.env["FIREBASE_PROJECT_ID"],
-	fireStorageBucket: process.env["FIREBASE_STORAGE_BUCKET"],
+	fireProjectId: "fir-crops-main",
+	fireStorageBucket: "fir-crops-main.firebasestorage.app",
 	fireMessageSenderId: process.env["FIREBASE_MESSAGE_SENDER_ID"],
 	fireAppId: process.env["FIREBASE_APP_ID"]
 };
