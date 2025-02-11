f3dbe49a8aed1384156cc97857c5386e05582d11
ismailramjean <ismailramjean@gmail.com>
2025-02-11 17:00:25 +0400

    & added environment variables for firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,6 +8,16 @@ import { getAuth, provideAuth } from '@angular/fire/auth';
 import {provideCharts, withDefaultRegisterables} from "ng2-charts";
 import {getStorage, provideStorage} from "@angular/fire/storage";
 import {FIREBASE_OPTIONS} from "@angular/fire/compat";
+import {environment} from "../environments/environment";
+
+const firebaseConfig = {
+	apiKey: environment.fireApiKey,
+	authDomain: environment.fireAuthDomain,
+	projectId: environment.fireProjectId,
+	storageBucket: environment.fireStorageBucket,
+	messagingSenderId: environment.fireMessageSenderId,
+	appId: environment.fireAppId,
+};
 
 export const appConfig: ApplicationConfig = {
 	providers: [provideRouter(routes),
