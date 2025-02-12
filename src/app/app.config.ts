b973acbd26561dd89af2a11d5037d631cfccbdd8
ismailramjean <ismailramjean@gmail.com>
2025-02-12 23:07:04 +0400

    & added config fixes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,15 +8,14 @@ import { getAuth, provideAuth } from '@angular/fire/auth';
 import {provideCharts, withDefaultRegisterables} from "ng2-charts";
 import {getStorage, provideStorage} from "@angular/fire/storage";
 import {FIREBASE_OPTIONS} from "@angular/fire/compat";
-import {environment} from "../environments/environment";
 
 const firebaseConfig = {
-	apiKey: environment.fireApiKey,
-	authDomain: environment.fireAuthDomain,
-	projectId: environment.fireProjectId,
-	storageBucket: environment.fireStorageBucket,
-	messagingSenderId: environment.fireMessageSenderId,
-	appId: environment.fireAppId,
+	apiKey: "AIzaSyCelSmWQonlcvl8VKYn5aQ9W_5D2lNqplk",
+	authDomain: "fir-crops-main.firebaseapp.com",
+	projectId: "fir-crops-main",
+	storageBucket: "fir-crops-main.firebasestorage.app",
+	messagingSenderId: "701697537930",
+	appId: "1:701697537930:web:80296f1010423a43a3ba73"
 };
 
 export const appConfig: ApplicationConfig = {
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,4 @@
 export const environment = {
 	production: false,
 	apiUrl: "http://localhost:8080",  // Development API URL
-	fireApiKey: '#{FIREBASE_API_KEY}',
-	fireAuthDomain: '#{FIREBASE_AUTH_DOMAIN}',
-	fireProjectId: "fir-crops-main",
-	fireStorageBucket: "fir-crops-main.firebasestorage.app",
-	fireMessageSenderId: '#{FIREBASE_MESSAGE_SENDER_ID}',
-	fireAppId: '#{FIREBASE_APP_ID}'
 };
