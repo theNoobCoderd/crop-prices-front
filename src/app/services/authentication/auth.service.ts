9c56125c51f4bd83412acccab0216481d826ff5d
ismailramjean <ismailramjean@gmail.com>
2025-08-28 15:15:36 +0400

    & removed unused methods

--- src/app/services/authentication/auth.service.ts
+++ src/app/services/authentication/auth.service.ts
@@ -18,30 +18,13 @@ import {UserService} from "../user/user.service";
 export class AuthService {
 	firebaseAuth = inject(Auth);
 	firebaseUser$ = user(this.firebaseAuth);
-	confirmationResult: ConfirmationResult | null = null;
 
 	_userService = inject(UserService);
 
 	constructor() { }
 
-	register(email: string, username: string, password: string): Observable<UserCredential> {
-		const promise = createUserWithEmailAndPassword(this.firebaseAuth, email, password)
-			.then((response) => {
-				updateProfile(response.user, {displayName: username});
-				return response;
-			}
-			);
-
-		return from(promise);
-	}
-
 	loginWithGoogle(): Observable<UserCredential> {
 		const promise = signInWithPopup(this.firebaseAuth, new GoogleAuthProvider());
-		return from(promise);
-	}
-
-	login(email: string, password: string): Observable<UserCredential> {
-		const promise = signInWithEmailAndPassword(this.firebaseAuth, email, password);
 		return from(promise);
 	}
 
@@ -49,30 +32,5 @@ export class AuthService {
 		const promise = signOut(this.firebaseAuth);
 		this._userService.logout();
 		return from(promise);
-	}
-
-	sendOTP(phoneNumber: string, recaptchaVerifier: RecaptchaVerifier): Observable<void> {
-		const promise = signInWithPhoneNumber(this.firebaseAuth, phoneNumber, recaptchaVerifier)
-			.then((confirmationResult) => {
-				console.log(confirmationResult);
-				this.confirmationResult = confirmationResult;
-			});
-		return from(promise);
-	}
-
-	verifyOTP(confirmationResult: ConfirmationResult, otp: string): Observable<UserCredential> {
-		const promise = confirmationResult.confirm(otp);
-		return from(promise);
-	}
-
-	getRecaptchaVerifier(containerId: string): RecaptchaVerifier {
-		return new RecaptchaVerifier(this.firebaseAuth, containerId, {
-			'size': 'invisible', // or 'normal' depending on your needs
-			// @ts-expect-error: Suppress implicit 'any' type error
-			'callback': (response) => {
-				// reCAPTCHA solved, allow signInWithPhoneNumber.
-				console.log(response);
-			}
-		});
 	}
 }
