5a13b375288cef0a19cac412b95abec1f53f72ad
ismailramjean <ismailramjean@gmail.com>
2025-08-28 13:45:03 +0400

    & bunch of changes  to historic feature

--- src/app/components/historic/historic.component.ts
+++ src/app/components/historic/historic.component.ts
@@ -1,44 +1,52 @@
-import {Component, OnInit} from "@angular/core";
+import {Component, OnDestroy} from "@angular/core";
 import {HistoricService} from "../../services/historic/historic.service";
-import {Observable, of} from "rxjs";
+import {BehaviorSubject, Observable, of, Subject, takeUntil, timer} from "rxjs";
 import {Vegetable} from "../../models/vegetable.model";
 import {ChartConfiguration, ChartOptions} from "chart.js";
 import {BaseChartDirective} from "ng2-charts";
-import {GRAPH_ONE} from "../../../assets/mocks/mock-data";
+import {GRAPH_TWO} from "../../../assets/mocks/mock-data";
 import {FormBuilder, FormGroup, ReactiveFormsModule} from "@angular/forms";
 import {DROP_DOWN_VALUE} from "../../constants/drop-down-values";
 import {DropDownComponent} from "../lib/drop-down/drop-down.component";
-import {NgForOf} from "@angular/common";
+import {MatTableModule} from "@angular/material/table";
+import {HistoryGraphModel} from "../../models/history-graph.model";
+import {Type} from "../../models/type.enum";
+import {DATE_RANGES} from "../../constants/date-ranges";
+import {AsyncPipe, NgForOf} from "@angular/common";
+import {average} from "@angular/fire/firestore";
+import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
+import {MatSelectModule} from "@angular/material/select";
 
 @Component({
-    selector: "app-historic",
+	selector: "app-historic",
 	imports: [
 		BaseChartDirective,
 		ReactiveFormsModule,
 		DropDownComponent,
-		NgForOf
+		MatTableModule,
+		NgForOf,
+		AsyncPipe,
+		MatProgressSpinnerModule,
+		MatSelectModule
 	],
-    templateUrl: "./historic.component.html",
-    styleUrl: "./historic.component.less"
+	templateUrl: "./historic.component.html",
+	standalone: true,
+	styleUrl: "./historic.component.less"
 })
-export class HistoricComponent implements OnInit {
+export class HistoricComponent implements OnDestroy {
 
+	// chart settings
 	lineChartData: ChartConfiguration<'line'>['data'] | undefined;
-
 	barChartData: ChartConfiguration<'bar'>['data'] | undefined;
-
 	averageRevenueChart: ChartConfiguration<'line'>['data'] | undefined;
-
-	historyParamForm: FormGroup;
-	timeOptions = [
-		{ label: '7 Days', value: '7' },
-		{ label: '30 Days', value: '30' },
-		{ label: '90 Days', value: '90' },
-		{ label: 'All Time', value: '1000' }
-	];
 
 	// @ts-ignore
 	lineChartOptions: ChartOptions<'line'> = {
+		plugins: {
+			legend: {
+				position: "bottom",
+			}
+		},
 		responsive: true,
 		maintainAspectRatio: false,
 		spanGaps: true,
@@ -67,8 +75,12 @@ export class HistoricComponent implements OnInit {
 			intersect: false
 		},
 	};
-
 	barChartOptions: ChartOptions<'bar'> = {
+		plugins: {
+			legend: {
+				position: "bottom",
+			}
+		},
 		responsive: true,
 		maintainAspectRatio: false,
 		interaction: {
@@ -103,7 +115,21 @@ export class HistoricComponent implements OnInit {
 		},
 	};
 
-	lineChartLegend = true;
+	// table settings
+	displayedColumns: string[] = ['date', 'lowPrice', 'highPrice', 'averagePrice', 'totalSold', 'revenue'];
+	dataSource: Vegetable[] = [];
+
+	historyParamForm: FormGroup;
+
+	cropHistory$: Observable<HistoryGraphModel> | undefined;
+	lowestPrice$ = new BehaviorSubject<number>(0);
+	highestPrice$ = new BehaviorSubject<number>(0);
+	averagePrice$ = new BehaviorSubject<number>(0);
+
+	isLoading$ = new BehaviorSubject<boolean>(false);
+	hasData$ = new BehaviorSubject<boolean>(false);
+
+	private _destroy$ = new Subject<void>();
 
 	constructor(private _historicService: HistoricService, private _formBuilder: FormBuilder) {
 		this.historyParamForm = this._formBuilder.group({
@@ -114,96 +140,96 @@ export class HistoricComponent implements OnInit {
 		console.log(this.historyParamForm.get('timeRange')?.value);
 	}
 
-	crops$: Observable<Vegetable[]> | undefined;
-
-	ngOnInit(): void {
+	ngOnDestroy(): void {
+		this._destroy$.next();
+		this._destroy$.complete();
 	}
 
 	onSubmit(): void {
+		this.hasData$.next(false);
+		this.isLoading$.next(true);
+
 		const formValue = this.historyParamForm.value;
 		console.log(formValue);
 
+
 		// this.crops$ = this._historicService.getHistoricByName("Angive");
-		this.crops$ = of(GRAPH_ONE);
-		this.crops$.subscribe((crops => {
-			var limitedDates = crops.slice(1, 10);
-			var dates = limitedDates.map((crop) => crop.date);
-			var averagePrices = limitedDates
-				.map((crop) => crop.averagePrice === 0 ? null : crop.averagePrice);
+		this.cropHistory$ = of(GRAPH_TWO);
+		this.cropHistory$.pipe(takeUntil(this._destroy$)).subscribe((crops => {
 
-			var highPrices = limitedDates
-				.map((crop) => crop.highPrice === 0 ? null : crop.highPrice);
+			setTimeout(()=>{
+				this.lowestPrice$.next(crops.lowestPrice);
+				this.highestPrice$.next(crops.highestPrice);
+				this.averagePrice$.next(crops.averagePrice);
 
-			var lowPrices = limitedDates
-				.map((crop) => crop.lowPrice === 0 ? null : crop.lowPrice);
+				this.dataSource = this._transformData(crops);
 
-			var totalSold = limitedDates.map((crop) => crop.totalSold);
+				this.lineChartData = {
+					labels: crops.dates,
+					datasets: [
+						{
+							data: crops.mostCommonPrices,
+							label: 'Average',
+							tension: 0.3,
+							borderColor: 'rgba(59, 130, 246, 1)',
+							borderWidth: 1,
+						},
+						{
+							data: crops.highestPrices,
+							label: 'High',
+							tension: 0.3,
+							borderColor: 'rgba(130, 202, 157, .6)',
+							borderWidth: 1,
+						},
+						{
+							data: crops.lowestPrices,
+							label: 'Low',
+							tension: 0.3,
+							borderColor: 'rgba(246,181,59, .6)',
+							borderWidth: 1,
+						}
+					]
+				};
 
-			var averageRevenu = limitedDates.map((crop) => crop.averagePrice * crop.totalSold);
+				this.barChartData = {
+					labels: crops.dates,
+					datasets: [
+						{
+							type: "bar" as const,
+							data: crops.totalSoldList,
+							label: 'total sold',
+							yAxisID: "y"
+						},
+						{
+							// @ts-ignore
+							type: "line" as const,
+							spanGaps: true,
+							data: crops.mostCommonPrices,
+							tension: 0.3,
+							label: 'average',
+							yAxisID: "y1"
+						}
+					]
+				}
 
-			this.lineChartData = {
-				labels: dates,
-				datasets: [
-					{
-						data: averagePrices,
-						label: 'Average',
-						tension: 0.3,
-						borderColor: 'rgba(59, 130, 246, 1)',
-						borderWidth: 1,
-					},
-					{
-						data: highPrices,
-						label: 'High',
-						tension: 0.3,
-						borderColor: 'rgba(130, 202, 157, .6)',
-						borderWidth: 1,
-					},
-					{
-						data: lowPrices,
-						label: 'Low',
-						tension: 0.3,
-						borderColor: 'rgba(246,181,59, .6)',
-						borderWidth: 1,
-					}
-				]
-			};
+				this.averageRevenueChart = {
+					labels: crops.dates,
+					datasets: [
+						{
+							data: crops.revenue,
+							label: 'Revenue',
+							tension: 0.3,
+							borderColor: 'rgba(59, 130, 246, 1)',
+							borderWidth: 1,
+							fill: true,
+							backgroundColor: 'rgba(142,180,246, 0.3)'
+						}
+					]
+				};
 
-			this.barChartData = {
-				labels: dates,
-				datasets: [
-					{
-						type: "bar" as const,
-						data: totalSold,
-						label: 'total sold',
-						yAxisID: "y"
-					},
-					{
-						// @ts-ignore
-						type: "line" as const,
-						spanGaps: true,
-						data: averagePrices,
-						tension: 0.3,
-						label: 'average',
-						yAxisID: "y1"
-					}
-				]
-			}
-
-			this.averageRevenueChart = {
-				labels: dates,
-				datasets: [
-					{
-						data: averageRevenu,
-						label: 'Revenu',
-						tension: 0.3,
-						borderColor: 'rgba(59, 130, 246, 1)',
-						borderWidth: 1,
-						fill: true,
-						backgroundColor: 'rgba(142,180,246, 0.3)'
-					}
-				]
-			};
-
+				this.isLoading$.next(false);
+				this.hasData$.next(true);
+			}, 1000)
 		}));
 	}
 
@@ -211,5 +237,22 @@ export class HistoricComponent implements OnInit {
 		this.historyParamForm.get('timeRange')?.setValue(value);
 	}
 
+	private _transformData(data: HistoryGraphModel): Vegetable[] {
+		return data.dates.map((date, index) => ({
+			id: index + 1, // or generate a unique ID if needed
+			name: data.vegetableName,
+			unitOfMeasure: data.unit,
+			totalSold: data.totalSoldList[index],
+			lowPrice: data.lowestPrices[index],
+			highPrice: data.highestPrices[index],
+			averagePrice: data.mostCommonPrices[index], // Using mostCommon as average
+			type: Type.VEGETABLE, // Assuming it's always vegetable
+			date: date,
+			revenue: data.revenue[index],
+		}));
+	}
+
 	protected readonly DROP_DOWN_VALUE = DROP_DOWN_VALUE;
+	protected readonly DATE_RANGES = DATE_RANGES;
+	protected readonly average = average;
 }
