f216a474e16ddbedaad4344c535cc0d62516d34b
ismailramjean <ismailramjean@gmail.com>
2025-09-22 22:01:01 +0400

    & changes for dashboard-addon

---
+++ src/assets/mocks/mock-premium-user.ts
@@ -0,0 +1,17 @@
+export const MockPremiumUser: any = {
+	userId: "",
+	purchases: [
+		{
+			cropName: "Angive",
+			rangeStartDate: "01.01.2024",
+			rangeEndDate: "01.01.2025",
+			rawData: ""
+		},
+		{
+			cropName: "Tomato",
+			rangeStartDate: "01.01.2024",
+			rangeEndDate: "01.01.2025",
+			rawData: ""
+		}
+	]
+}
--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,13 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import posthog from "posthog-js";
 
-posthog.init(
-	'phc_iVhxqcsJlUSE3HT568Yt3cCYvmTuxQtpTmSMANJF75X',
-	{
-		api_host:'https://us.i.posthog.com',
-		person_profiles: 'identified_only'
-	}
-)
+// posthog.init(
+// 	'phc_iVhxqcsJlUSE3HT568Yt3cCYvmTuxQtpTmSMANJF75X',
+// 	{
+// 		api_host:'https://us.i.posthog.com',
+// 		person_profiles: 'identified_only'
+// 	}
+// )
 
 bootstrapApplication(AppComponent, appConfig)
 	.catch((err) => console.error(err));
