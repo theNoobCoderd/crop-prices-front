a4cc5ec22ecc269bc2e89adfb1edc327ae60a8ef
ismailramjean <ismailramjean@gmail.com>
2025-12-08 21:57:53 +0400

    & temporary commit

--- src/app/components/marketplace/marketplace.component.ts
+++ src/app/components/marketplace/marketplace.component.ts
@@ -31,7 +31,8 @@ export class MarketplaceComponent implements OnDestroy {
 		this._marketPlaceListing.getAllListing()
 			.pipe(takeUntil(this._destroy$))
 			.subscribe(results => {
-				this.marketPlaceItems$.next(results);
+				const first = this.tempMethod(results);
+				this.marketPlaceItems$.next(first);
 				this.listingsLoaded$.next(true);
 			});
 	}
@@ -39,6 +40,20 @@ export class MarketplaceComponent implements OnDestroy {
 	ngOnDestroy(): void {
 		this._destroy$.next();
 		this._destroy$.complete();
+	}
+
+	private tempMethod(listing: MarketplaceListing[]): MarketplaceListing[] {
+		let id = "f625352a-e23f-4bc7-b1db-c3a3ed21c8a8";
+		const index = listing.findIndex(item => item.id === id);
+		if (index > -1) {
+			const [item] = listing.splice(index, 1); // remove the item
+			listing.unshift(item); // put it at the start
+		}
+
+		const item = listing.find(x => x.id === id);
+		if (!item) return listing;
+
+		return [item, ...listing.filter(x => x.id !== id)];
 	}
 
 	mockMarketPlaceItems: MarketplaceListing[] = MARKETPLACE_LISTING
