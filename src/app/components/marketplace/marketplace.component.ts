d7afe288d18cdd7138119487f39c99e798292cc5
ismailramjean <ismailramjean@gmail.com>
2025-12-14 11:38:34 +0400

    & removed temp code

--- src/app/components/marketplace/marketplace.component.ts
+++ src/app/components/marketplace/marketplace.component.ts
@@ -4,7 +4,7 @@ import {MARKETPLACE_LISTING} from "../../../assets/mocks/mock-data";
 import {MarketplaceListing} from "../../models/marketplace-listing.model";
 import {AddCropComponent} from "./add-crop/add-crop.component";
 import {ListingService} from "../../services/listing/listing.service";
-import {async, BehaviorSubject, Subject, takeUntil} from "rxjs";
+import {BehaviorSubject, Subject, takeUntil} from "rxjs";
 import {AsyncPipe} from "@angular/common";
 import {NgxSkeletonLoaderModule} from "ngx-skeleton-loader";
 
@@ -31,8 +31,7 @@ export class MarketplaceComponent implements OnDestroy {
 		this._marketPlaceListing.getAllListing()
 			.pipe(takeUntil(this._destroy$))
 			.subscribe(results => {
-				const first = this.tempMethod(results);
-				this.marketPlaceItems$.next(first);
+				this.marketPlaceItems$.next(results);
 				this.listingsLoaded$.next(true);
 			});
 	}
@@ -40,20 +39,6 @@ export class MarketplaceComponent implements OnDestroy {
 	ngOnDestroy(): void {
 		this._destroy$.next();
 		this._destroy$.complete();
-	}
-
-	private tempMethod(listing: MarketplaceListing[]): MarketplaceListing[] {
-		let id = "f625352a-e23f-4bc7-b1db-c3a3ed21c8a8";
-		const index = listing.findIndex(item => item.id === id);
-		if (index > -1) {
-			const [item] = listing.splice(index, 1); // remove the item
-			listing.unshift(item); // put it at the start
-		}
-
-		const item = listing.find(x => x.id === id);
-		if (!item) return listing;
-
-		return [item, ...listing.filter(x => x.id !== id)];
 	}
 
 	mockMarketPlaceItems: MarketplaceListing[] = MARKETPLACE_LISTING
